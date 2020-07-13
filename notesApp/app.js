9fed49d565b2f7e5f400c3d9c8460440f94f5010
dscrane <daegancrane@gmail.com>
2020-07-13 11:14:00 -0400

    added yargs and started working on the command line args

--- notesApp/app.js
+++ notesApp/app.js
@@ -1,13 +1,68 @@
-// const validator = require('validator');
+const fs = require('fs');
+const yargs = require('yargs');
+const chalk = require('chalk');
 const { successLog, errorLog, variableLog, textLog } = require('./chalkLogs');
 const getNotes = require('./notes.js');
 
-const msg = getNotes();
-textLog(msg);
+// customize yargs version
+yargs.version('1.1.0');
 
-// console.log(validator.isEmail('andrew@email.com'));
+/* Notes app tasks */
+// add, remove, read, list
 
-successLog('API Request', 'The call was successful');
-errorLog('Request Failed', 'API could not be found');
+//create add command
+yargs.command({
+  command: 'add',
+  /* command: <nameOfApp> */
+  describe: '\x1b[36mAdd a new note\x1b[0m',
+  /* describe: <descriptionOfCommand> */
+  builder: {
+    /* builder: {<allOfTheSupportedOptions>} */
+    title: {
+      /* <option>: <howTheOptionWorks> */
+      describe: 'Note Title',
+      /* describe: <descriptionOfOption */
+      demandOption: true,
+      type: 'string',
+    },
+    message: {
+      describe: 'Note content',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler: argv => {
+    console.log(
+      `${chalk.cyan('Adding new note:')}\n ${chalk.yellow('title:')} ${
+        argv.title
+      }\n ${chalk.yellow('message:')} ${argv.message}`
+    );
+  },
+});
 
-variableLog('scoreCardData', ['one', 'two', 'three']);
+//create remove command
+yargs.command({
+  command: 'remove',
+  describe: '\x1b[36mRemove an existing note\x1b[0m',
+  handler: () => {
+    textLog('Removing a note');
+  },
+});
+
+yargs.command({
+  command: 'read',
+  describe: '\x1b[36mRead notes\x1b[0m',
+  handler: () => {
+    textLog('Reading a note');
+  },
+});
+
+yargs.command({
+  command: 'list',
+  describe: '\x1b[36mList all notes\x1b[0m',
+  handler: () => {
+    textLog('Listing all notes');
+  },
+});
+
+yargs.parse();
--- notesApp/oldWork/old__app.js
+++ notesApp/oldWork/old__app.js
@@ -4,3 +4,31 @@ const { name, add, nameToHex } = require('./utils.js');
 fs.writeFileSync('notes.txt', `My name is ${name} \n`);
 
 fs.appendFileSync('notes.txt', `The number I like a lot is ${nameToHex()}`);
+
+// const validator = require('validator');
+const { successLog, errorLog, variableLog, textLog } = require('./chalkLogs');
+const getNotes = require('./notes.js');
+
+const msg = getNotes();
+textLog(msg);
+
+// console.log(validator.isEmail('andrew@email.com'));
+
+variableLog('scoreCardData', process.argv);
+
+// command line parsing
+const command = process.argv[2];
+const optionsInputs = process.argv.slice(3);
+
+const options = optionsInputs.map(optionInput => {
+  const data = optionInput.slice(2);
+  const splitData = data.split('=');
+  const optionData = { title: splitData[0], value: splitData[1] };
+  return optionData;
+});
+
+console.log(optionData);
+
+if (command === 'add') {
+  console.log('Adding Note');
+}
