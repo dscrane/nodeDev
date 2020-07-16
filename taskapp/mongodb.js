a644cd5f3340b07829b047af7465f0fa99db6447
dscrane <daegancrane@gmail.com>
2020-07-16 12:18:39 -0400

    beginning working on the 3rd section of the course with mongodb

---
+++ taskapp/mongodb.js
@@ -0,0 +1,18 @@
+const mongodb = require('mongodb');
+
+const MongoClient = mongodb.MongoClient;
+
+const connectionURL = 'mongodb://127.0.0.1:27017';
+const databaseName = 'task-app';
+
+MongoClient.connect(
+  connectionURL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err, client) => {
+    if (err) {
+      return console.log('there was an error');
+    }
+
+    console.log('connected to');
+  }
+);
