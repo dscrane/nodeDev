b3cf1b043c7a912d34abc93db93c6f1b487be575
dscrane <daegancrane@gmail.com>
2020-07-17 11:16:04 -0400

    got mongodb server set up and working and beginning setting up CRUD operations

--- taskapp/mongodb.js
+++ taskapp/mongodb.js
@@ -13,6 +13,64 @@ MongoClient.connect(
       return console.log('there was an error');
     }
 
-    console.log('connected to');
+    const db = client.db(databaseName);
+    /* 
+    db.collection('users').insertOne(
+      {
+        name: 'Daegan',
+        age: '20s',
+      },
+      (err, result) => {
+        if (err) {
+          return console.log('Unable to insert user');
+        }
+
+        console.log(result.ops);
+      }
+    ); */
+
+    /* db.collection('users').insertMany(
+      [
+        {
+          name: 'Jen',
+          age: 30,
+        },
+        {
+          name: 'Sally',
+          age: 25,
+        },
+      ],
+      (err, result) => {
+        if (err) {
+          return console.log('Could not insert users');
+        }
+
+        console.log(result.ops);
+      }
+    ); */
+
+    db.collection('tasks').insertMany(
+      [
+        {
+          description: 'Get Mongodb working',
+          completed: true,
+        },
+        {
+          desctiption: 'Finish Node Dev Course',
+          completed: false,
+        },
+        {
+          desctiption: 'Eat breakfast',
+          completed: false,
+        },
+      ],
+      (err, result) => {
+        if (err) {
+          return console.log('There was an error inserting these tasks');
+        }
+
+        console.log(result.ops);
+      }
+    );
   }
 );
