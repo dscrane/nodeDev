baef28f46c52f50eb4c648a09c2c871636d60935
dscrane <daegancrane@gmail.com>
2020-07-26 15:03:59 -0400

    chat features with timestamps are now set up

--- chatapp/public/js/chat.js
+++ chatapp/public/js/chat.js
@@ -1,14 +1,81 @@
 const socket = io();
 
+// Elements
+const messageForm = document.querySelector('#message-form');
+const messageFormInput = messageForm.querySelector('input');
+const locationCta = document.querySelector('#location-cta');
+const messageFormButton = messageForm.querySelector('button');
+const messages = document.querySelector('#messages');
+
+//Templates
+const messageTemplate = document.querySelector('#message-template').innerHTML;
+const locationMessageTemplate = document.querySelector(
+  '#location-message-template'
+).innerHTML;
+
+// Socket event for when a user wishes to send a messsage to other users
 socket.on('message', message => {
-  console.log(message);
+  /* console.log(message); */
+
+  const html = Mustache.render(messageTemplate, {
+    message: message.text,
+    createdAt: moment(message.createdAt).format('h:mm a'),
+  });
+  messages.insertAdjacentHTML('beforeend', html);
 });
 
-const messageForm = document.querySelector('#message-form');
+// Socket event for when a user wishes to send their location
+socket.on('locationMessage', message => {
+  // Create the html for the mustache template for sending the location
+  const html = Mustache.render(locationMessageTemplate, {
+    location: message.locationUrl,
+    createdAt: moment(message.createdAt).format('h:mm a'),
+  });
+  messages.insertAdjacentHTML('beforeend', html);
+});
+
+// The text form for creating and sending messages
 messageForm.addEventListener('submit', e => {
   e.preventDefault();
 
-  const message = e.target.elements.message.value;
+  messageFormButton.setAttribute('disabled', 'disabled');
 
-  socket.emit('sendMessage', message);
+  const message = e.target.elements.message.value;
+  // Socket event for when a user sends a message to the server
+  socket.emit('sendMessage', message, err => {
+    // The event acknowledgement and error handling
+    messageFormButton.removeAttribute('disabled');
+    messageFormInput.value = '';
+    messageFormInput.focus();
+    if (err) {
+      return console.log(err);
+    }
+
+    console.log('Delivered');
+  });
+});
+
+// The button for sending the users current location
+locationCta.addEventListener('click', () => {
+  // Message for users who do no have the geolocation available in their browser
+  if (!navigator.geolocation) {
+    return alert('Geolocation is not supported by your browser.');
+  }
+
+  locationCta.setAttribute('disabled', 'disabled');
+  // Getting the user location when the button is clicked
+  navigator.geolocation.getCurrentPosition(position => {
+    /* console.log(position); */
+    const location = {
+      lat: position.coords.latitude,
+      long: position.coords.longitude,
+    };
+
+    // Socket even for sending the user location to the server
+    socket.emit('sendLocation', location, err => {
+      // Event acknowledgement and error handling
+      locationCta.removeAttribute('disabled');
+      console.log(err);
+    });
+  });
 });
--- chatapp/src/index.js
+++ chatapp/src/index.js
@@ -2,6 +2,8 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const Filter = require('bad-words');
+const { generateMessage } = require('./util/messages');
 
 const app = express();
 const server = http.createServer(app);
@@ -14,10 +16,31 @@ app.use(express.static(publicDirectory));
 
 io.on('connection', socket => {
   console.log('New websocket connection');
-  socket.emit('message', 'Welcome!');
+  socket.emit('message', generateMessage('Welcome!'));
+  socket.broadcast.emit('message', generateMessage('A new user has joined!'));
 
-  socket.on('sendMessage', message => {
-    io.emit('message', message);
+  socket.on('sendMessage', (message, callback) => {
+    const filter = new Filter();
+
+    if (filter.isProfane(message)) {
+      return callback('Profanity is not allowed in this chatroom');
+    }
+
+    io.emit('message', generateMessage(message));
+    callback('Delivered');
+  });
+
+  socket.on('sendLocation', ({ lat, long }, callback) => {
+    io.emit(
+      'locationMessage',
+      generateMessage(`https://google.com/maps?q=${lat},${long}`)
+    );
+
+    callback('Location Shared!');
+  });
+
+  socket.on('disconnect', () => {
+    io.emit('message', generateMessage('A user has left!'));
   });
 });
 
